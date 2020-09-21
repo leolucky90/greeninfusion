399a4933a8105690ea2402c7507fb92180dc7ed1
Son Vu <vt.sagis@gmail.com>
2020-09-21 19:47:05 +1000

    Content updated

--- script/content-generator.js
+++ script/content-generator.js
@@ -1,11 +1,11 @@
 let coffeeProds = [
   {
     id: "prod1",
-    name: "Prod1 Lolololol",
+    name: "AROMAS Decaf Coffee Beans",
     price: 30, // Price per 100g
     image: "assets/images/items/prod1.jpg",
-    description1: "",
-    description2: "",
+    description1: "Using the Swiss Water Processing method, these decaf coffee beans from Colombia have a mild citric acidity paired with notes of dark chocolate and a medium to light body. ",
+    description2: "The Swiss Water method of caffeine removal involves bathing the green beans in water that is highly saturated with the main flavour compounds of coffee. This situation sets up an osmosis reaction where only the caffeine content is removed the green beans, leaving all of the flavour compounds still in the green beans! ",
     sizeOptions: ["100", "500", "1000"],
     grindOptions: ["Whole beans", "Aero press", "Drip"],
     prefSize: 2, // Preferred size index
@@ -13,91 +13,91 @@ let coffeeProds = [
   },
   {
     id: "prod2",
-    name: "Kumbaya",
-    price: 45,
-    image: "assets/images/items/prod2.jpg",
-    description1: "",
-    description2: "",
+    name: "OPEN PLAN Espresso Roast Coffee Beans",
+    price: 36,
+    image: "assets/images/items/prod5.jpg",
+    description1: "I stumbled upon Open Plan Coffee Roasters in North Perth almost by accident, since then they’ve become a firm favourite for my morning coffee. ",
+    description2: "Tim from Open Plan blends and roasts the beans himself and you can really tell the difference from high quality freshly roasted beans. It’s a great coffee and we’re excited to bring you another product from a local producer that we believe in.",
     sizeOptions: ["100", "500", "1000"],
     grindOptions: ["Whole beans", "Aero press", "Drip"],
-    prefSize: 1, 
+    prefSize: 0,
     prefGrind: 0
   },
   {
     id: "prod3",
-    name: "Kumbaya ya ya",
-    price: 45,
+    name: "TARISMA Coconut Coffee",
+    price: 21,
     image: "assets/images/items/prod8.jpg",
     description1: "",
     description2: "",
     sizeOptions: ["100", "500", "1000"],
     grindOptions: ["Whole beans", "Aero press", "Drip"],
-    prefSize: 0, 
+    prefSize: 0,
     prefGrind: 1
   },
   {
     id: "prod4",
-    name: "Heyho",
-    price: 45,
+    name: "ADORA Whole Coffee Beans",
+    price: 33,
     image: "assets/images/items/prod4.jpg",
-    description1: "",
-    description2: "",
+    description1: "We’re turning the tides on terrible decaf coffee from lazy cafes and anti-decaf coffee marketing.",
+    description2: "From the mountains of Mexico comes this 'Mountain Water Washed' Decaf Coffee. Not only is it decaffeinated but it is also 100% certified Organic and Rainforest Alliance coffee.",
     sizeOptions: ["100", "500", "1000"],
     grindOptions: ["Whole beans", "Aero press", "Drip"],
-    prefSize: 1, 
+    prefSize: 1,
     prefGrind: 2
   },
   {
     id: "prod5",
-    name: "Best coffee",
-    price: 45,
-    image: "assets/images/items/prod5.jpg",
-    description1: "",
-    description2: "",
+    name: "SICILIA Espresso Mocha Coffee Beans",
+    price: 40,
+    image: "assets/images/items/prod2.jpg",
+    description1: "ESPRESSO MOCHA: Coffee beans sourced from the high regions in southern Africa. This blend contains toffee and almond notes with subtle hints of melon and blackberry",
+    description2: "It has a rich syrup body with a bitter-sweet cocoa finish. It is a very satisfying cup for coffee lovers that seek a medium-strong mocha flavour.",
     sizeOptions: ["100", "500", "1000"],
     grindOptions: ["Whole beans", "Aero press", "Drip"],
-    prefSize: 0, 
+    prefSize: 1,
     prefGrind: 0
   },
   {
     id: "prod6",
-    name: "Hory sheet",
-    price: 45,
+    name: "GIANCARLO Verona Blend Coffee Beans",
+    price: 24,
     image: "assets/images/items/prod6.jpg",
-    description1: "",
-    description2: "",
+    description1: "The aroma is of intense dried fruit, and the flavours have a nutty almond character and pronounced earthiness. The long, smooth aftertaste is of dried fruit and a disctinct earthy note.",
+    description2: "As an espresso, the result is a muted sweetness and heavy body. When combined with milk the earthy pungency of the Robusta coffee rises above the sweetness of the milk to produce a memorable, strong coffee experience.",
     sizeOptions: ["100", "500", "1000"],
     grindOptions: ["Whole beans", "Aero press", "Drip"],
-    prefSize: 1, 
+    prefSize: 1,
     prefGrind: 1
   },
   {
     id: "prod7",
-    name: "Chiekcn",
-    price: 45,
+    name: "D'TIME Espresso Energy Bar",
+    price: 12,
     image: "assets/images/items/prod7.jpg",
-    description1: "",
-    description2: "",
+    description1: "d’TIME Espresso Energy Bar ™ contains quality almonds, pumpkin seeds, melon seeds, sultana and dark chocolate – the best compliment to the nuts.",
+    description2: "d’TIME Espresso Energy Bar ™ is Halal and MeSTI Certified.",
     sizeOptions: ["100", "500", "1000"],
     grindOptions: ["Whole beans", "Aero press", "Drip"],
-    prefSize: 2, 
+    prefSize: 2,
     prefGrind: 2
   },
   {
     id: "prod8",
-    name: "Njam mzk",
-    price: 45,
+    name: "ITALCAFFÈ Gran Gusto Espresso Bar Coffee Beans",
+    price: 42,
     image: "assets/images/items/prod3.jpg",
-    description1: "",
-    description2: "",
+    description1: "A great coffee composed of a harmonious blending of coffees of the Arabica and Robusta species, accurately measured in order to offer you a great Italian Espresso with medium roast.",
+    description2: "Italcaffè coffee beans are perfect for perfect for espresso and bean to cup coffee machines. Italcaffè whole espresso bean is packaged in a protective atmosphere to conserve taste and freshness. This is a bag of 1kg with one-way degassing valve. These features guarantee to Italcaffè Gran Gusto a better preservation over time as well as greater quality of the whole bean coffee compared to the usual bags that you find on the market.",
     sizeOptions: ["100", "500", "1000"],
     grindOptions: ["Whole beans", "Aero press", "Drip"],
-    prefSize: 2, 
+    prefSize: 2,
     prefGrind: 0
   }
 ]
 
 
-$(function() {
+$(function () {
 
 });
